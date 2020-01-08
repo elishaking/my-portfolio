09fb1f76648d0764d3d89722bf9c6c3eee2e76fd
King Elisha <ek.chibueze@gmail.com>
2020-01-08 14:04:14 +0100

    new component

--- src/components/routes/Landing.js
+++ src/components/routes/Landing.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Container from '../Container';
 
 export default class Landing extends Component {
   render() {
     return (
-      <div>
+      <Container>
         <h1>Landing Page</h1>
-      </div>
+      </Container>
     );
   }
 }
