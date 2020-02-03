f72bc21506045748c5a1a96340d48623acfc932d
King Elisha <ek.chibueze@gmail.com>
2020-02-03 19:09:40 +0100

    tech stack

--- src/components/routes/Projects.js
+++ src/components/routes/Projects.js
@@ -20,49 +20,57 @@ const projects = [
     name: "BlazeHub: Social Networking App",
     source: blazehub,
     description: "BlazeHub is a platform for connecting people: A place to chat, follow interesting conversions and be a part of a growing community",
-    link: "https://blazehub.skyblazar.com"
+    link: "https://blazehub.skyblazar.com",
+    tech: ["React/Redux", "Node/Express", "Firebase", "GCP", "Jest/Supertest/GitHub Actions"]
   },
   {
     name: "Squaredemy: The most productive way to learn",
     source: squaredemy,
     description: "Squaredemy is an AI-driven learning platform that enhances learning productivity through customized curriculums that are generated based on fun and engaging interactions with the user",
-    link: "https://squaredemy.skyblazar.com"
+    link: "https://squaredemy.skyblazar.com",
+    tech: ["React/Redux", "Node/Express", "Firebase", "GCP", "Dart/Flutter", "SQLite3", "Jest/Supertest/GitHub Actions"]
   },
   {
     name: "CalcEn: Complex Calculator App",
     source: calcen,
     description: "CalcEn is a complex number calculator for android that handles arithmetic of complex expressions, matrices and functions",
-    link: "https://calcen.skyblazar.com"
+    link: "https://calcen.skyblazar.com",
+    tech: ["React/Redux", "Node/Express", "Firebase", "Ionic/Angular", "SQLite3"]
   },
   {
     name: "Gracelites International School",
     source: gracelites,
     description: "Gracelites is an outstanding Nursery/Primary committed to laying the crucial foundations that will set your child on the right path to all-round excellence spiritually, intellectually and physically",
-    link: "https://gracelites.skyblazar.com"
+    link: "https://gracelites.skyblazar.com",
+    tech: ["React/Redux", "Node/Express", "SQLite3"]
   },
   {
     name: "Nasa Xplorer",
     source: nasax,
     description: "Explore fascinating images of Space: Planets, Moons, Asteriods, Stars, Galaxies and more",
-    link: "https://nasax.skyblazar.com"
+    link: "https://nasax.skyblazar.com",
+    tech: ["React/Redux", "Node/Express", "SQLite3"]
   },
   {
     name: "Blaze Markdown",
     source: markdown,
     description: "Preview markdown and download as PDF or Image",
-    link: "https://markdown.skyblazar.com"
+    link: "https://markdown.skyblazar.com",
+    tech: ["React/Redux", "Node/Express"]
   },
   {
     name: "Dizt: Navigation App",
     source: dizt,
     description: "Dizt is a simple and intuitive mobile app that enables you to determine the road distance between two locations and the exact travel time using your car, the bus, your bicycle or by walking. You also get to see the transport fare if you are using a bus along some routes",
-    link: "https://dizt.skyblazar.com"
+    link: "https://dizt.skyblazar.com",
+    tech: ["React/Redux", "GCP", "Dart/Flutter"]
   },
   {
     name: "Quotz: Quotes App",
     source: quotz,
     description: "Quotz is an elegant mobile app that displays several mindfully selected quotes in a simple and elegant interface that can be used as a screensaver. It contains words of wisdom from several great men and woman who have had a great impact on the world",
-    link: "https://quotz.skyblazar.com"
+    link: "https://quotz.skyblazar.com",
+    tech: ["React/Redux", "Node/Express", "SQLite3", "GCP", "Dart/Flutter"]
   },
 ];
 
@@ -76,6 +84,7 @@ export default class Projects extends Component {
 
   closeModal = (e) => {
     this.setState({ showModal: false });
+
   };
 
   openModal = (e) => {
@@ -119,9 +128,17 @@ export default class Projects extends Component {
                     <small><strong>Contributor: </strong> Elisha King</small>
                   </a> */}
 
-                  <a href={project.link} rel="noopener noreferrer" target="_blank">
+                  <a className="project-link" href={project.link} rel="noopener noreferrer" target="_blank">
                     {project.name}
                   </a>
+
+                  {/* <div className="tech">
+                    {
+                      project.tech.map((t, i) => (
+                        <span>{t}{i !== project.tech.length - 1 && <span className="separator">&middot;</span>}</span>
+                      ))
+                    }
+                  </div> */}
                 </div>
               ))
             }
