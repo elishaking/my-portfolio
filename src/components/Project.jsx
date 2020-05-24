6efab0127776e53b98d5e494c0e28863be471ca8
King Elisha <ek.chibueze@gmail.com>
2020-05-24 15:31:00 +0100

    image loading

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,21 +1,34 @@
 import React from "react";
 import See from "./icons/See";
+import { useState } from "react";
 
 export default function Project({ project, openModal }) {
+  const [imageLoaded, updateImageLoaded] = useState(false);
+
+  const setImageLoaded = () => {
+    updateImageLoaded(true);
+  };
+
   return (
     <div className="project">
       <div className="image">
         <div>
+          {!imageLoaded && <div className="img-placeholder"></div>}
+
           <img
             src={project.source}
             alt={project.name}
             data-description={project.description}
+            onLoad={setImageLoaded}
+            style={{ display: !imageLoaded && "none" }}
           />
         </div>
 
-        <div className="hover" onClick={openModal}>
-          <See onClick={(e) => e.stopPropagation()} />
-        </div>
+        {imageLoaded && (
+          <div className="hover" onClick={openModal}>
+            <See onClick={(e) => e.stopPropagation()} />
+          </div>
+        )}
       </div>
 
       <a
