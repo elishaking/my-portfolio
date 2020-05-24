adea1436f5b9be899b54492d9432eb4b45dbe732
King Elisha <ek.chibueze@gmail.com>
2020-05-24 14:45:52 +0100

    routes ==> pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './App.scss';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.scss";
 
-import Landing from './components/routes/Landing';
-import About from './components/routes/About';
-import Projects from './components/routes/Projects';
+import Landing from "./components/pages/Landing";
+import About from "./components/pages/About";
+import Projects from "./components/pages/Projects";
 
 function App() {
   return (
