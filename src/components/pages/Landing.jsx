0ee798f2b8b7d955eeb80a2fae3604c473a3d9ad
King Elisha <ek.chibueze@gmail.com>
2020-05-24 14:58:44 +0100

    jsx, fonts, emojis

---
+++ src/components/pages/Landing.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import Container from "../Container";
+import Navbar from "../Navbar";
+
+export default class Landing extends Component {
+  openMail = () => {
+    window.open("mailto:ek.chibueze@gmail.com", "_blank");
+  };
+
+  render() {
+    return (
+      <Container className="landing">
+        <Navbar />
+
+        <div className="content">
+          <div className="left">
+            <h1>
+              Hi{" "}
+              <span role="img" aria-label="wave">
+                👋
+              </span>
+            </h1>
+            <h1 className="headline">
+              I am King Elisha <br />A <span>Full Stack</span> Software Enginner{" "}
+              <span>\></span>
+            </h1>
+            <p>
+              I love building elegant software solutions with <br />
+              beautiful <span>frontends</span> and robust <span>backends</span>
+            </p>
+            <button onClick={this.openMail}>
+              Contact Me{" "}
+              <span role="img" aria-label="wave">
+                👨‍💻
+              </span>
+            </button>
+          </div>
+        </div>
+      </Container>
+    );
+  }
+}
