9ea23199352bd03f1209431630e58b0ce8c2d292
King Elisha <ek.chibueze@gmail.com>
2020-05-24 16:23:55 +0100

    updated stylesheet

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,88 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import Github from "./icons/Github";
+import Dribbble from "./icons/Dribbble";
+import Medium from "./icons/Medium";
+import DevTo from "./icons/DevTo";
+import Linkedin from "./icons/Linkedin";
 
 export default function Navbar({ route = "home" }) {
   return (
     <nav className="navbar">
       <h1>King Elisha</h1>
 
+      <ul className="svg">
+        <div>
+          <a
+            href="https://github.com/elishaking"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Github />
+          </a>
+          <a
+            href="https://github.com/elishaking"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Dribbble />
+          </a>
+          <a
+            href="https://github.com/elishaking"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Linkedin />
+          </a>
+          <a
+            href="https://github.com/elishaking"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Medium />
+          </a>
+          <a
+            href="https://github.com/elishaking"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <DevTo />
+          </a>
+          <a
+            href="https://github.com/elishaking"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Medium alt />
+          </a>
+        </div>
+      </ul>
+
       <ul>
-        <Link to="/" className={route === "home" ? "active" : ""}>Home</Link>
-        <Link to="/projects" className={route === "projects" ? "active" : ""}>Projects</Link>
-        <Link to="/about" className={route === "about" ? "active" : ""}>About</Link>
-        {/* <Link to="/github" className={route === "github" ? "active" : ""}>Github</Link> */}
-        <a href="https://github.com/elishaking"
+        <Link to="/" className={`text ${route === "home" ? "active" : ""}`}>
+          Home
+        </Link>
+        <Link
+          to="/projects"
+          className={`text ${route === "projects" ? "active" : ""}`}
+        >
+          Projects
+        </Link>
+        <Link
+          to="/about"
+          className={`text ${route === "about" ? "active" : ""}`}
+        >
+          About
+        </Link>
+        <a
+          href="https://github.com/elishaking"
           target="_blank"
-          rel="noopener noreferrer">Github</a>
+          rel="noopener noreferrer"
+          className="text"
+        >
+          Github
+        </a>
       </ul>
     </nav>
-  )
+  );
 }
